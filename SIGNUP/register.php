8b98e65afbf35f03fe31dd3c275e3717ab6ee55b
Jomarie <jarreugse09@gmail.com>
2024-12-01 14:19:03 +0800

    joms commit

--- SIGNUP/register.php
+++ SIGNUP/register.php
@@ -1,6 +1,6 @@
 <?php
 session_start();
-require "/xampp/htdocs/Ease_Slip/assets/connection.php";
+require "/xampp/htdocs/EaseSlip/assets/connection.php";
 
 // Initialize response variable
 $response = '';
